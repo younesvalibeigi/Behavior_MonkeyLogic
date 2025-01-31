a1ced3d8a241c5fb7cd84259843e7ad18b8b887d
younesvalibeigi <younesvalibeigi@gmail.com>
2025-01-31 14:37:44 -0500

    some changes

--- 14_RFmap_StimShow_DMS/txtMaker_dms.m
+++ 14_RFmap_StimShow_DMS/txtMaker_dms.m
@@ -4,7 +4,7 @@ header_row = {'Condition', 'Frequency', 'Block', 'Timing File', 'TaskObject#1', 
 % Define the data rows
 data_rows = cell(10, length(header_row));
 fix = [0 0];
-rf = [-5 -2];
+rf = [-4 -3];
 pxperdeg = 36.039;
 img_size = [9.5 9.5]*pxperdeg;
 target_im_size = [10 10]*pxperdeg;
